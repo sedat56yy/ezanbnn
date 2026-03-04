217a7c6ad56b09547d05360293bbcba291953039
sedat56yy <sedatulug44@gmail.com>
2026-03-05 01:29:29 +0300

    Update config.py to remove tokens and add dependenciesk
    Removed sensitive information and added package dependencies.

--- config.py
+++ config.py
@@ -1,17 +1,3 @@
 
-# -*- coding: utf-8 -*-
-import os
-
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "8695515506:AAHmDqO0a7pdaBFJppwjtuSXQ40c47TPYvs")
-ADMIN_IDS    = [583994825]
-DATA_FILE    = "ezan_data.json"
-LOG_FILE     = "ezan_log.json"
-KANAL_URL    = "https://t.me/nikestoretr"
-KANAL_LABEL  = "📢 Kanal"
-KURUCU_URL   = "https://t.me/NikeCheatYeniden"
-KURUCU_LABEL = "👤 Kurucu"
-BOT_LOGO     = "https://i.hizliresim.com/ojtpq9s.jpg"
-SPAM_SURE    = 5
-SPAM_CACHE   = {}
-GNDR         = {}
-_VAKIT_CACHE = {}
+requests>=2.31.0
+pytz>=2024.1
